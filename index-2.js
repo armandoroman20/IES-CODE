9c068557345561a2706535ac8b579bd760e99ec1
Armando R <armando.roman.jr20@gmail.com>
2022-04-21 13:46:53 -0500

    Created carousel files / css that are compatible with Elementor

--- index-2.js
+++ index-2.js
@@ -71,3 +71,123 @@ $("#carousel-1").on("slide.bs.carousel", function (e) {
       }
     }
   });
+
+  $("#carousel-4").on("slide.bs.carousel", function (e) {
+    var $e = $(e.relatedTarget);
+
+    var idx = $e.index();
+    var itemsPerSlide = 3;
+    var totalItems = $(".carousel-4-item").length;
+
+    if (idx >= totalItems - (itemsPerSlide - 1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i = 0; i < it; i++) {
+        // append slides to end
+        if (e.direction == "left") {
+          $(".carousel-4-item" + ".col-lg-4")
+            .eq(i)
+            .appendTo(".carousel-4");
+        } else {
+          $(".carousel-4-item" + ".col-lg-4")
+            .eq(0)
+            .appendTo(".carousel-4");
+        }
+      }
+    }
+  });
+
+  $("#carousel-5").on("slide.bs.carousel", function (e) {
+    var $e = $(e.relatedTarget);
+
+    var idx = $e.index();
+    var itemsPerSlide = 3;
+    var totalItems = $(".carousel-5-item").length;
+
+    if (idx >= totalItems - (itemsPerSlide - 1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i = 0; i < it; i++) {
+        // append slides to end
+        if (e.direction == "left") {
+          $(".carousel-5-item" + ".col-lg-4")
+            .eq(i)
+            .appendTo(".carousel-5");
+        } else {
+          $(".carousel-5-item" + ".col-lg-4")
+            .eq(0)
+            .appendTo(".carousel-5");
+        }
+      }
+    }
+  });
+
+  $("#carousel-6").on("slide.bs.carousel", function (e) {
+    var $e = $(e.relatedTarget);
+
+    var idx = $e.index();
+    var itemsPerSlide = 3;
+    var totalItems = $(".carousel-6-item").length;
+
+    if (idx >= totalItems - (itemsPerSlide - 1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i = 0; i < it; i++) {
+        // append slides to end
+        if (e.direction == "left") {
+          $(".carousel-6-item" + ".col-lg-4")
+            .eq(i)
+            .appendTo(".carousel-6");
+        } else {
+          $(".carousel-6-item" + ".col-lg-4")
+            .eq(0)
+            .appendTo(".carousel-6");
+        }
+      }
+    }
+  });
+
+  $("#carousel-7").on("slide.bs.carousel", function (e) {
+    var $e = $(e.relatedTarget);
+
+    var idx = $e.index();
+    var itemsPerSlide = 3;
+    var totalItems = $(".carousel-7-item").length;
+
+    if (idx >= totalItems - (itemsPerSlide - 1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i = 0; i < it; i++) {
+        // append slides to end
+        if (e.direction == "left") {
+          $(".carousel-7-item" + ".col-lg-4")
+            .eq(i)
+            .appendTo(".carousel-7");
+        } else {
+          $(".carousel-7-item" + ".col-lg-4")
+            .eq(0)
+            .appendTo(".carousel-7");
+        }
+      }
+    }
+  });
+
+  $("#carousel-8").on("slide.bs.carousel", function (e) {
+    var $e = $(e.relatedTarget);
+
+    var idx = $e.index();
+    var itemsPerSlide = 3;
+    var totalItems = $(".carousel-8-item").length;
+
+    if (idx >= totalItems - (itemsPerSlide - 1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i = 0; i < it; i++) {
+        // append slides to end
+        if (e.direction == "left") {
+          $(".carousel-8-item" + ".col-lg-4")
+            .eq(i)
+            .appendTo(".carousel-8");
+        } else {
+          $(".carousel-8-item" + ".col-lg-4")
+            .eq(0)
+            .appendTo(".carousel-8");
+        }
+      }
+    }
+  });
